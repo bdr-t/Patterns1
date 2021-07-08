9638216b9d5635db17a2e1ae644c54b6de05fc6f
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-07-08 13:01:52 +0200

    callaback_hell

---
+++ callback_hell.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+
+const inbox = path.join(__dirname, "inbox");
+const outbox = path.join(__dirname, "outbox");
+const reverseText = str =>
+  str
+    .split("")
+    .reverse()
+    .join("");
+
+
+function getFile() {
+  return fs.readdirSync(inbox, (error, files) => {
+    if (error) return console.log("Error: Folder inaccessible");
+    return files
+  });
+}
+
+function readFile(paso1) {
+  paso1.forEach(file => {
+    fs.readFile(path.join(inbox, file), "utf8", (error, data) => {
+      if (error) return console.log("Error: File error");
+      writeFile(file, data)
+    });
+  });
+}
+
+function writeFile(file, data) { 
+  fs.writeFile(path.join(outbox, file), reverseText(data), error => {
+    if (error) return console.log("Error: File could not be saved!");
+    console.log(`${file} was successfully saved in the outbox!`);
+  });
+}
+
+
+let paso1 = getFile()
+readFile(paso1)
+
